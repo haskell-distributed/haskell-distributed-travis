1ff64f43e73c485a570e10260ebd5951f9f96292
Tim Watson <watson.timothy@gmail.com>
2014-02-21 23:46:43 +0000

    Tweaks for ghc-7.4.x

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ CB=`cat HEAD_BRANCH`
 ./travis/scripts/install-deps.sh\
   && echo "============================================================"\
   && echo "Installing to $CB"\
-  && make -j BRANCH=$CB BASE_DIR=`pwd` install-deps
+  && make -j BRANCH=$CB BASE_DIR=`pwd` CABAL_EXTRA=--force-reinstalls install-deps
