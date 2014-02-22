539f9997e5ee966113daf8bd605d2e88c1feb3cd
Tim Watson <watson.timothy@gmail.com>
2014-02-22 00:02:49 +0000

    For now, skip the check/sdist parts

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-if [[ $GHCVER == "head" || -n "$SKIP_HADDOCK" ]]
-  then
-    DOCBUILD="echo 'Skipping docs...'"
-  else
-    DOCBUILD="$CABAL haddock"
-fi
+#if [[ $GHCVER == "head" || -n "$SKIP_HADDOCK" ]]
+#  then
+#    DOCBUILD="echo 'Skipping docs...'"
+#  else
+#    DOCBUILD="$CABAL haddock"
+#fi
 
-make BRANCH=`cat HEAD_BRANCH` BASE_DIR=`pwd` test \
-  && $DOCBUILD \
-  && $CABAL check \
-  && $CABAL sdist
+make BRANCH=`cat HEAD_BRANCH` BASE_DIR=`pwd` test 
+#\
+#  && $DOCBUILD \
+#  && $CABAL check \
+#  && $CABAL sdist
